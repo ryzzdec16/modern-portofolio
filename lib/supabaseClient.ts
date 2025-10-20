0bd918fdf7e1d3fb99e81bf474c8a86b77f5eeba
ryzzdec16 <ryzzdec16@gmail.com>
2025-10-20 19:21:14 +0700

    Fix Supabase client for production

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,10 @@
-// lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export default supabase
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('SUPABASE_URL or SUPABASE_ANON_KEY is missing');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
